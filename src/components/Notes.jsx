5400fb3252665aad3f0de389d1406b0075d427e6
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-07-08 00:57:16 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is First Note</h1>
+      <p>
+        If you're looking for random paragraphs, you've come to the right place.
+        When a random word or a random sentence isn't quite enough, the next
+        logical step is to find a random paragraph.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
